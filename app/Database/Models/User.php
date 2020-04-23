66c5d3353ea41f3e744a73f2bd5ecd7d4f2ac3d3
jongwoo Yoo <dbwhddn10@gmail.com>
2020-04-23 11:57:28 +0900

    remove not necessary relation in model

--- app/Database/Models/User.php
+++ app/Database/Models/User.php
@@ -3,18 +3,7 @@
 namespace App\Database\Models;
 
 use App\Database\Model;
-use App\Database\Models\Card;
-use App\Database\Models\CardFlip;
 use App\Database\Models\FacePhoto;
-use App\Database\Models\UserIdealTypeKwdPvt;
-use App\Database\Models\Match;
-use App\Database\Models\UserSelfKwdPvt;
-use App\Database\Models\ProfilePhoto;
-use App\Database\Models\Popularity;
-use App\Database\Models\Reply;
-use App\Database\Models\Role;
-use App\Database\Models\Ticket;
-use App\Database\Models\User;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Auth\MustVerifyEmail;
 use Illuminate\Auth\Passwords\CanResetPassword;
@@ -68,76 +57,9 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
         return ['facePhoto'];
     }
 
-    public function cardFlips()
-    {
-        return $this->hasMany(CardFlip::class, 'user_id', 'id');
-    }
-
-    public function chooserCards()
-    {
-        return $this->hasMany(Card::class, 'chooser_id', 'id');
-    }
-
     public function facePhoto()
     {
         return $this->hasOne(FacePhoto::class, 'user_id', 'id');
-    }
-
-    public function userIdealTypeKwdPvts()
-    {
-        return $this->hasMany(UserIdealTypeKwdPvt::class, 'user_id', 'id');
-    }
-
-    public function matches()
-    {
-        if ( $this->{static::GENDER} === static::GENDER_MAN )
-        {
-            return $this->hasMany(Match::class, 'man_id', 'id');
-        }
-        else if ( $this->{static::GENDER} === static::GENDER_WOMAN )
-        {
-            return $this->hasMany(Match::class, 'woman_id', 'id');
-        }
-    }
-
-    public function userSelfKwdPvts()
-    {
-        return $this->hasMany(UserSelfKwdPvt::class, 'user_id', 'id');
-    }
-
-    public function profilePhotos()
-    {
-        return $this->hasMany(ProfilePhoto::class, 'user_id', 'id');
-    }
-
-    public function tickets()
-    {
-        return $this->hasMany(Ticket::class, 'writer_id', 'id');
-    }
-
-    public function receivedPopularities()
-    {
-        return $this->hasMany(Popularity::class, 'receiver_id', 'id');
-    }
-
-    public function replies()
-    {
-        return $this->hasMany(Reply::class, 'writer_id', 'id');
-    }
-
-    public function roles()
-    {
-        return $this->hasMany(Role::class, 'user_id', 'id');
-    }
-
-    public function sentPopularities()
-    {
-        return $this->hasMany(Popularity::class, 'sender_id', 'id');
-    }
-
-    public function shownerCards()
-    {
-        return $this->hasMany(Card::class, 'showner_id', 'id');
     }
 
 }
