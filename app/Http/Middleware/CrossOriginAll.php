749cbd85056507f624c5ac65c884a9e4fe144373
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-30 19:33:49 +0900

    set Access-Control-Allow-Origin to *

--- app/Http/Middleware/CrossOriginAll.php
+++ app/Http/Middleware/CrossOriginAll.php
@@ -8,7 +8,7 @@ class CrossOriginAll {
     {
         $response = $next($request);
 
-        $response->header('Access-Control-Allow-Origin', 'http://localhost:4200');
+        $response->header('Access-Control-Allow-Origin', '*');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
         $response->header('Access-Control-Allow-Headers','Accept, Authorization, Content-Type, DNT, Origin, Referer, User-Agent, X-XSRF-TOKEN, X-CSRF-TOKEN');
         $response->header('Access-Control-Allow-Credentials',' true');
