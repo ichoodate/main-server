f41b31c2dff54885e0af170d185ea310684e1d7a
jongwoo Yoo <dbwhddn10@gmail.com>
2021-08-08 14:17:37 +0900

    remove unit test files

--- tests/Functional/_TestCase.php
+++ tests/Functional/_TestCase.php
@@ -3,39 +3,28 @@
 namespace Tests\Functional;
 
 use App\Database\Model;
+use Faker\Generator as Faker;
 use Illuminate\Extend\Service;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Tests\_TestCase as TestCase;
+use Tests\TestCase;
 
-class _TestCase extends TestCase {
-
+/**
+ * @internal
+ * @coversNothing
+ */
+class _TestCase extends TestCase
+{
     use DatabaseMigrations;
     use WithoutMiddleware;
 
     public $environment = 'functional';
 
-    public function assertResult($expect)
+    public function setUp(): void
     {
-        $serv   = $this->runService();
-        $result = $serv->data()->get('result');
-        $errors = $serv->totalErrors()->all();
+        parent::setUp();
 
-        $this->assertEquals([], $errors, implode(',', $errors));
-        $this->assertEquals($expect, $result);
-    }
-
-    public function assertPersistence($model, $existCount = 1)
-    {
-        $attrs = $model->getAttributes();
-        $query = inst(get_class($model))->query();
-
-        foreach ( $attrs as $attr => $value )
-        {
-            $query->where($attr, $value);
-        }
-
-        $this->assertEquals($existCount, $query->count());
+        $this->faker = app(Faker::class);
     }
 
     public function assertDeletion($model)
@@ -45,42 +34,54 @@ class _TestCase extends TestCase {
 
     public function assertError($msg)
     {
-        $serv   = $this->runService();
+        $serv = $this->runService();
         $errors = $serv->totalErrors()->all();
 
         $this->assertContains($msg, $errors, implode(',', $errors));
     }
 
-    public function assertResultWithPersisting($expects)
+    public static function assertException($executeClosure, $expectClosure = null)
     {
-        $serv   = $this->runService();
+        try {
+            call_user_func($executeClosure);
+        } catch (\Exception $e) {
+            if (null != $expectClosure) {
+                call_user_func($expectClosure, $e);
+            }
+
+            static::success();
+
+            return;
+        }
+
+        static::fail();
+    }
+
+    public function assertPersistence($model, $existCount = 1)
+    {
+        $attrs = $model->getAttributes();
+        $query = inst(get_class($model))->query();
+
+        foreach ($attrs as $attr => $value) {
+            $query->where($attr, $value);
+        }
+
+        $this->assertEquals($existCount, $query->count());
+    }
+
+    public function assertResult($expect)
+    {
+        $serv = $this->runService();
         $result = $serv->data()->get('result');
         $errors = $serv->totalErrors()->all();
 
-        if ( $expects instanceof Model )
-        {
-            $this->assertInstanceOf(Model::class, $result);
-
-            $expects = collect([$expects]);
-            $result  = collect([$result]);
-        }
-
         $this->assertEquals([], $errors, implode(',', $errors));
-        $this->assertEquals(get_class($expects), get_class($result));
-
-        foreach ( $result as $i => $model )
-        {
-            $expect = $expects[$i];
-
-            $this->assertInstanceOf(Model::class, $model);
-            $this->assertEquals(get_class($expect), get_class($model));
-            $this->assertEquals([], array_diff($expect->toArray(), $model->toArray()));
-        }
+        $this->assertEquals($expect, $result);
     }
 
     public function assertResultWithFinding($expectId)
     {
-        $serv   = $this->runService();
+        $serv = $this->runService();
         $result = $serv->data()->get('result');
         $errors = $serv->totalErrors()->all();
 
@@ -90,23 +91,22 @@ class _TestCase extends TestCase {
 
     public function assertResultWithListing($expectIds)
     {
-        $serv   = $this->runService();
+        $serv = $this->runService();
         $result = $serv->data()->get('result');
         $errors = $serv->totalErrors()->all();
 
         $this->assertEquals([], $errors, implode(',', $errors));
 
-        foreach ( $expectIds as $expectId )
-        {
+        foreach ($expectIds as $expectId) {
             $this->assertContains($expectId, $result->modelKeys());
         }
     }
 
     public function assertResultWithPaging($expectIds)
     {
-        $serv    = $this->runService();
-        $result  = $serv->data()->get('result')->modelKeys();
-        $errors  = $serv->totalErrors()->all();
+        $serv = $this->runService();
+        $result = $serv->data()->get('result')->modelKeys();
+        $errors = $serv->totalErrors()->all();
 
         sort($expectIds);
         sort($result);
@@ -115,21 +115,46 @@ class _TestCase extends TestCase {
         $this->assertEquals($result, $expectIds);
     }
 
+    public function assertResultWithPersisting($expects)
+    {
+        $serv = $this->runService();
+        $result = $serv->data()->get('result');
+        $errors = $serv->totalErrors()->all();
+
+        if ($expects instanceof Model) {
+            $this->assertInstanceOf(Model::class, $result);
+
+            $expects = collect([$expects]);
+            $result = collect([$result]);
+        }
+
+        $this->assertEquals([], $errors, implode(',', $errors));
+        $this->assertEquals(get_class($expects), get_class($result));
+
+        foreach ($result as $i => $model) {
+            $expect = $expects[$i];
+
+            $this->assertInstanceOf(Model::class, $model);
+            $this->assertEquals(get_class($expect), get_class($model));
+            $this->assertEquals([], array_diff($expect->toArray(), $model->toArray()));
+        }
+    }
+
     public function assertResultWithReturning($expect)
     {
-        $serv    = $this->runService();
-        $result  = $serv->data()->get('result');
-        $errors  = $serv->totalErrors()->all();
+        $serv = $this->runService();
+        $result = $serv->data()->get('result');
+        $errors = $serv->totalErrors()->all();
 
         $this->assertEquals([], $errors, implode(',', $errors));
         $this->assertEquals($expect, $result);
     }
 
-    public function getQuery($serv)
+    public static function factory($modelClass)
     {
-        $builder = $serv->data()->get('query');
-        $addSlashes = str_replace('?', "'?'", $builder->toSql());
-        $q = vsprintf(str_replace('?', '%s', $addSlashes), $builder->getBindings());
+        $path = str_replace('App\\Database\\Models\\', '', $modelClass);
+
+        return inst('Database\\Factories\\Model\\'.$path.'Factory');
     }
 
     public function getHttpMethod()
@@ -143,6 +168,13 @@ class _TestCase extends TestCase {
         return strtoupper(array_pop($class));
     }
 
+    public function getQuery($serv)
+    {
+        $builder = $serv->data()->get('query');
+        $addSlashes = str_replace('?', "'?'", $builder->toSql());
+        $q = vsprintf(str_replace('?', '%s', $addSlashes), $builder->getBindings());
+    }
+
     public function getResponse()
     {
         $method = $this->getHttpMethod();
@@ -150,10 +182,33 @@ class _TestCase extends TestCase {
         return $this->call($method, $url = $this->url, $parameters = $this->inputs, $cookies = [], $files = [], $server = [], $content = null);
     }
 
+    public static function invokeMethod($object, $method, $args, $public = true)
+    {
+        if (is_string($method)) {
+            $reflection = new \ReflectionClass($object);
+
+            $method = $reflection->getMethod($method);
+
+            if ($public) {
+                $method->setAccessible(true);
+            }
+
+            return $method->invokeArgs($object, $args);
+        }
+        $method = \Closure::bind($method, $object);
+
+        return call_user_func_array($method, $args);
+    }
+
+    public static function ref(...$args)
+    {
+        return new \ReflectionClass(...$args);
+    }
+
     public function runService()
     {
         $response = $this->getResponse();
-        $content  = $response->getOriginalContent();
+        $content = $response->getOriginalContent();
 
         $this->assertTrue(Service::isCanServicify($content));
 
@@ -168,18 +223,28 @@ class _TestCase extends TestCase {
         auth()->setUser($user);
     }
 
+    public function setInputParameter($key, $value)
+    {
+        $this->inputs[$key] = $value;
+    }
+
     public function setRouteParameter($key, $value)
     {
         $replace = $value;
         $subject = $this->url;
-        $search  = '{'.$key.'}';
+        $search = '{'.$key.'}';
 
         $this->url = str_replace($search, $replace, $subject);
     }
 
-    public function setInputParameter($key, $value)
+    public static function success()
     {
-        $this->inputs[$key] = $value;
+        static::assertTrue(true);
+    }
+
+    public static function uniqueString()
+    {
+        return str_random(50);
     }
 
     public function when()
@@ -188,7 +253,7 @@ class _TestCase extends TestCase {
 
         auth()->logout();
 
-        $this->url    = $this->uri;
+        $this->url = $this->uri;
         $this->inputs = [];
 
         app('db')->beginTransaction();
@@ -197,6 +262,4 @@ class _TestCase extends TestCase {
 
         app('db')->rollback();
     }
-
 }
-
