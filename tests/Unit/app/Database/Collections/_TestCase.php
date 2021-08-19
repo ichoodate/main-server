4edfa1c952da2c48ed68b283604165b6d3f93fb7
jongwoo Yoo <dbwhddn10@gmail.com>
2021-08-20 02:48:23 +0900

    delete Unit testing of Services

--- tests/Unit/app/Database/Collections/_TestCase.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Tests\Unit\App\Database\Collections;
-
-use Tests\Unit\Database\_TestCase as TestCase;
-
-class _TestCase extends TestCase {
-
-}
--- tests/Unit/app/Http/Controllers/ApiControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Tests\Unit\App\Http\Controllers;
-
-use Tests\Unit\_TestCase;
-
-class ApiControllerTest extends _TestCase {
-
-}
