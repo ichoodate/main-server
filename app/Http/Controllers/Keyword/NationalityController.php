1ad1407479bc5954a07a6417e2df9100ab4ad38b
jongwoo Yoo <dbwhddn10@gmail.com>
2021-08-24 02:11:07 +0900

    add NationalityListingService

--- app/Http/Controllers/Keyword/NationalityController.php
+++ app/Http/Controllers/Keyword/NationalityController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers\Keyword;
 
 use App\Http\Controller;
 use App\Services\Keyword\Nationality\NationalityFindingService;
+use App\Services\Keyword\Nationality\NationalityListingService;
 
 class NationalityController extends Controller
 {
+    public static function index()
+    {
+        return [NationalityListingService::class];
+    }
+
     public static function show()
     {
         return [NationalityFindingService::class];
---
+++ app/Services/Keyword/Nationality/NationalityListingService.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Services\Keyword\Nationality;
+
+use App\Models\Keyword\Nationality;
+use FunctionalCoding\ORM\Eloquent\Service\ListService;
+use FunctionalCoding\Service;
+
+class NationalityListingService extends Service
+{
+    public static function getArrBindNames()
+    {
+        return [];
+    }
+
+    public static function getArrCallbackLists()
+    {
+        return [];
+    }
+
+    public static function getArrLoaders()
+    {
+        return [
+            'available_expands' => function () {
+                return [];
+            },
+
+            'model_class' => function () {
+                return Nationality::class;
+            },
+        ];
+    }
+
+    public static function getArrPromiseLists()
+    {
+        return [];
+    }
+
+    public static function getArrRuleLists()
+    {
+        return [];
+    }
+
+    public static function getArrTraits()
+    {
+        return [
+            ListService::class,
+        ];
+    }
+}
